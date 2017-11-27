750df643e7b71342a1b629c691d2445d136cf54d
Matthew Choi <matthewchoi7@gmail.com>
2017-11-27 16:44:03 +1100

    adding CSS minification plugin

--- webpack.prod.js
+++ webpack.prod.js
@@ -3,11 +3,15 @@ const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const common = require('./webpack.common.js');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const WebpackMonitor = require('webpack-monitor');
+// CSS minification plugin
+const OptimizeCSSAssets = require('optimize-css-assets-webpack-plugin');
 
 module.exports = merge(common, {
   plugins: [
     // UglifyJSPlugin handles minification and tree shaking.
     new UglifyJSPlugin(),
+    // OptimizeCSSAssets handles CSS minification
+    new OptimizeCSSAssets(),
     // WebpackMonitor saves stats about your product build.
     // To view data about your build and it's size, set launch to true.
     new WebpackMonitor({
